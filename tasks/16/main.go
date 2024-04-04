32ffca0d1175999fb928f68a4f48fb30683862e4
fredacha <fredacha@student.21-school.ru>
2024-04-04 17:24:37 +0300

    added comments

--- tasks/16/main.go
+++ tasks/16/main.go
@@ -4,23 +4,42 @@ import (
 	"fmt"
 )
 
+// Сама функция сортировки. Сравниваем по условию элементы по левому и правому индексу и меняем их местами
 func partition(arr []int, low, high int) ([]int, int) {
+	// Создаем опорный элемент (самый правый индекс)
 	pivot := arr[high]
+
+	// Сохраняем минимальный индекс (самый левый)
+	// По этому индексу в дальнейшем будет делиться массив
 	i := low
+
+	// Итерируемся по слайсу и если текущий элемент ме	ньше опорного, то меняем их местами
+	// и увеличиваем индекс i
 	for j := low; j < high; j++ {
 		if arr[j] < pivot {
 			arr[i], arr[j] = arr[j], arr[i]
 			i++
 		}
+		// fmt.Println(arr)
 	}
+	
+	// Меняем самый правый индекс местами с i-тым индексов
 	arr[i], arr[high] = arr[high], arr[i]
+	// fmt.Println()
+
+	// Возвращаем отсортированный слайс и индекс по которому
+	// будем делить слайс
 	return arr, i
 }
 
 func quickSort(arr []int, low, high int) []int {
 	if low < high {
 		var p int
+		// Сортируем
 		arr, p = partition(arr, low, high)
+		
+		// Условно делим массив на два подмассива
+		// и рекурсивно вызываем функцию quickSort
 		arr = quickSort(arr, low, p-1)
 		arr = quickSort(arr, p+1, high)
 	}
@@ -28,7 +47,7 @@ func quickSort(arr []int, low, high int) []int {
 }
 
 func startSort(data []int) []int {
-	return quickSort(data, 0, len(data)-1)
+	return quickSort(data, 0, len(data)-1) 
 }
 
 func main() {
--- tasks/17/main.go
+++ tasks/17/main.go
@@ -5,11 +5,20 @@ import (
 )
 
 func binarySearch(data []int, target int) (int, bool) {
+	// Сохраняем минимальный индекс
 	low := 0
+	
+	// Сохраняем максимальный индекс
 	high := len(data) - 1
+
+
 	for low <= high {
+		// Вычисляем индекс центрального элемента
 		mid := (low + high) / 2
 
+		// Если таргет равен центральному элементу, возвращаем индекс центрального элемента и индикатор true
+		// В противном случае, в зависимости от условия, уменьшаем максимальный
+		// или увеличиваем минимальный индекс
 		if target == data[mid] {
 			return mid, true
 		} else if target < data[mid] {
@@ -18,13 +27,15 @@ func binarySearch(data []int, target int) (int, bool) {
 			low = mid + 1
 		}
 	}
+
+	// Если ничего не найдено возвращаем 0 и индикатор false
 	return 0, false
 }
 
 func main() {
 	data := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-	i, ok := binarySearch(data, 20)
+	i, ok := binarySearch(data, 4)
 	if !ok {
 		fmt.Println("Sorry")
 	} else {
